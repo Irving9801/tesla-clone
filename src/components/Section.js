1cf6aa2cf7ff4b0e22e9e4a43b0162517e6ad4da
IRVING_VASQUEZ <ivasquez@hypernovalabs.com>
2021-10-05 22:24:55 -0500

    ADD:redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,23 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 function Section({
   backgroundImg,
   titles,
   description,
   leftBtnText,
-  RightBtnText,
+  rightBtnText,
 }) {
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{titles}</h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{titles}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {RightBtnText && <RightButton>{RightBtnText}</RightButton>}
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>
+                 {leftBtnText}
+            </LeftButton>
+            { rightBtnText &&
+             <RightButton>
+                 {rightBtnText}
+              </RightButton>
+            }
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
@@ -27,6 +38,7 @@ function Section({
 export default Section;
 
 const Wrap = styled.div`
+  z-index: 10;
   width: 100vw;
   height: 100vh;
   background-size: cover;
@@ -42,6 +54,7 @@ const Wrap = styled.div`
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
+  z-index:-1;
 `;
 const ButtonGroup = styled.div`
   display: flex;
